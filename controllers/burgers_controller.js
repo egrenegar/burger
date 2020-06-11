b74ffb08c839d70b272a15d7ab5aadf414a3a124
Emily Renegar <egrenegar@gmail.com>
2020-06-10 21:52:22 -0400

    wrote api routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,4 +1,37 @@
 const express = require('express');
 const burger = require('../models/burger');
+const router = express.Router();
 
-// Create the router for the app, and export the router at the end of your file
+// write Routes here
+router.get('/', function(req, res) {
+    burger.selectAll(function(data) {
+        var dictionary = {
+            bugers: data
+        };
+        console.log(dictionary);
+        res.render('index', dictionary);
+    });
+});
+
+router.post('/api/burgers', function(req, res) {
+    burger.insertOne('name', req.body.name, function(result) {
+        console.log(result)
+        res.json({id: result.insertId })
+    });
+});
+
+router.put('/api/burgers/:id', function(req, res) {
+    const condition = 'id = ' + req.params.id;
+
+    console.log('condition', condition);
+
+    burger.updateOne({devoured: req.body.devoured}, condition, function(result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
