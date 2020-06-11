bbf1b1b2e9f641529ee92f1e9c92827266a3eac3
Emily Renegar <egrenegar@gmail.com>
2020-06-10 21:51:40 -0400

    defined orm functions

--- config/orm.js
+++ config/orm.js
@@ -2,18 +2,31 @@ const connection = require('./connection');
 // create the methods that will execute the necessary MySQL commands in the controllers.
 // These are the methods you will need to use in order to retrieve and store data in your database.
 
-orm.selectAll(
-    connection.query('')
-);
+const orm = {
+    selectAll: function (whatToSelect, table) {
+        const queryString = 'SELECT ?? FROM ??';
+        connection.query(queryString, [whatToSelect, table], function (err, res) {
+            if (err) throw err;
+            console.table(res);
+        });
+    },
+
+    insertOne: function () {
+        const queryString = '';
+        connection.query(queryString, [], function (err, res) {
+            if (err) throw err;
+            console.table(res);
+        });
+    },
+
+    updateOne: function () {
+        const queryString = '';
+        connection.query(queryString, [], function (err, res) {
+            if (err) throw err;
+            console.table(res);
+        });
+    }
+}
 
 
-orm.insertOne(
-    connection.query('')
-);
-
-
-orm.updateOne(
-    connection.query('')
-);
-
-module.exports = ();
+module.exports = orm;
